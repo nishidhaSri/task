aedef490172914b19f5f2af8b60c65dfd7677b3b
Nishidha Sri <44313138+MeekMo@users.noreply.github.com>
2020-05-23 15:25:22 +0530

    Updates

---
+++ precache-manifest.41363d93fa2140c77a4eb7e74c9b755d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0681cf04abb9a85b4347889ee9eec62",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "35ae58922d9feb7fa422",
+    "url": "/task/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "eb03173f5d6cd4f74f33",
+    "url": "/task/static/css/main.172377cf.chunk.css"
+  },
+  {
+    "revision": "35ae58922d9feb7fa422",
+    "url": "/task/static/js/2.c2e409e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/task/static/js/2.c2e409e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb03173f5d6cd4f74f33",
+    "url": "/task/static/js/main.78fcdf9c.chunk.js"
+  },
+  {
+    "revision": "ac3ef2771d37fa2ff0d5",
+    "url": "/task/static/js/runtime-main.09f078db.js"
+  }
+]);
